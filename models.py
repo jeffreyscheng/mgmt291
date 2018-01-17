fcb5dbb2986ca7371f9af68d0aa83808a41b42b2
Jeffrey Cheng <jeffch@wharton.upenn.edu>
2018-01-17 18:00:14 -0500

    added oddball logic

--- models.py
+++ models.py
@@ -50,6 +50,7 @@ class Roleplay(db.Model):
     section_name = db.Column(db.String(64), db.ForeignKey('section.name'))
     group_size = db.Column(db.Integer)
     attendees = db.relationship('AttendanceRecord', backref='parent_roleplay', lazy='dynamic')
+
     # __table_args__ = (ForeignKeyConstraint([name], [Section.name]), {})
 
     # def __init__(self, idx, name, number, assignments, started, section_name, group_size):
@@ -87,18 +88,33 @@ class Roleplay(db.Model):
         students = [record.student_name for record in records]
         encoding = {rank: students[rank - 1] for rank in range(1, len(students) + 1)}
         reverse_encoding = {encoding[key]: key for key in encoding}
-        encounters = pd.DataFrame(0, index=students, columns=students)
+        all_encounters = pd.DataFrame(0, index=students, columns=students)
         previous_roleplays = Roleplay.query.with_parent(self.parent_section).all()
         previous_roleplays.remove(self)
         print(previous_roleplays)
+        min_tracker = {student: 0 for student in students}
         for roleplay in previous_roleplays:
             for group in eval(roleplay.assignments):
                 all_pairs = list(itertools.combinations(group, 2))
                 for pair in all_pairs:
                     stud1, stud2 = pair
                     if stud1 in students and stud2 in students:
-                        encounters.loc[stud1, stud2] += 1
-                        encounters.loc[stud2, stud1] += 1
+                        all_encounters.loc[stud1, stud2] += 1
+                        all_encounters.loc[stud2, stud1] += 1
+                        min_tracker[stud1] += 1
+                        min_tracker[stud2] += 2
+        encounters = all_encounters.copy()
+        # if class not evenly divisible
+        remainder = len(students) % self.group_size
+        dropped = []
+        while remainder != 0:
+            encounters = all_encounters.copy()
+            min_student = min(min_tracker, key=min_tracker.get)
+            encounters.drop([min_student])
+            encounters.drop([min_student], axis=1, inplace=True)
+            del min_tracker[min_student]
+            dropped.append(min_student)
+            remainder -= 1
         print(encounters)
         pref_lists = {}
         for student in students:
@@ -107,20 +123,33 @@ class Roleplay(db.Model):
             pref_list.remove(student)
             pref_lists[reverse_encoding[student]] = [reverse_encoding[student] for student in pref_list]
         print(pref_lists)
-        if self.group_size == 2: # TODO odd
+        assignments = []
+        if self.group_size == 2:
             matching = stableroomate(pref_lists)
             print(matching)
-            assignments = []
             for i in range(1, len(students) + 1):
                 if i in matching:
                     pair = encoding[i], encoding[matching[i]]
                     assignments.append(pair)
                     del matching[matching[i]]
                     del matching[i]
-            print(assignments)
-            self.assignments = str(assignments)
         elif self.group_size >= 3:
-            pass # TODO brute force, odd
+            pass  # TODO brute force
+        added = []
+        while dropped:  # takes care of oddballs
+            curr = dropped.pop()
+            remaining_groups = [group for group in assignments if group not in added]
+            possible_matches = {group: 0 for group in assignments if group not in added}
+            for group in remaining_groups:
+                for member in group:
+                    possible_matches[group] += all_encounters.loc[curr, member]
+            best_group = min(possible_matches, possible_matches.get)
+            assignments.remove(best_group)
+            best_group = best_group + (curr,)
+            assignments.append(best_group)
+            added.append(best_group)
+        print(assignments)
+        self.assignments = str(assignments)
         db.session.commit()
         print("FINISHED ASSIGNING")
 
@@ -129,6 +158,7 @@ class AttendanceRecord(db.Model):
     id = db.Column(db.String(64), primary_key=True)
     student_name = db.Column(db.String(64))
     roleplay_id = db.Column(db.String(64), db.ForeignKey('roleplay.id'))
+
     # __table_args__ = (ForeignKeyConstraint([id], [Roleplay.id]), {})
 
     # def __init__(self, idx, student_name, roleplay):
